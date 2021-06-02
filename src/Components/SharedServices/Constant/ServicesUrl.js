282911c65ef9f316468f209b0b4a92e793ca33b5
Anand Agrawal <anand24agrawal@gmail.com>
2021-06-02 18:20:50 +0530

    service url change

--- src/Components/SharedServices/Constant/ServicesUrl.js
+++ src/Components/SharedServices/Constant/ServicesUrl.js
@@ -1,6 +1,5 @@
-//const baseUrl = 'https://r6ic7nlj8f.execute-api.ap-south-1.amazonaws.com/Prod/api/HRMS/';
-const baseUrl = "https://localhost:44317/api/HRMS/";
-//const baseUrl = 'https://s68mul9gu7.execute-api.ap-south-1.amazonaws.com/Prod/api/HRMS/';
+const baseUrl = "https://y08yf1jxc6.execute-api.ap-south-1.amazonaws.com/Prod/";
+//const baseUrl = "https://localhost:44317/api/HRMS/";
 
 export const ServiceUrl = {
   VALIDATE_USER: baseUrl + "Login/ValidateUser",
